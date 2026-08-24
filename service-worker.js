15feedc0a0e0706c9f5cf8c1fc3fd45fcddd8669
PrimeSyntax <minoucoding@gmail.com>
2026-08-24 19:09:53 +0200

    .

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION = "2026-08-24-v13.00.0";
+const CACHE_VERSION = "2026-08-24-v13.50.0";
 const CACHE_PREFIX = "sprite-locker-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${CACHE_VERSION}`;
 const ASSET_CACHE = `${CACHE_PREFIX}assets-${CACHE_VERSION}`;
